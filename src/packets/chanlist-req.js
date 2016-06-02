d2cae996df28835db5c84a5db62e254b3efab8d1
Andrew Vy <andrew@andrewvy.com>
2016-06-01 17:40:40 -0700

    :bug: Math.floor

--- src/packets/chanlist-req.js
+++ src/packets/chanlist-req.js
@@ -5,7 +5,7 @@ ChanlistReq.prototype.deserialize = function(data) {
 }
 
 ChanlistReq.prototype.respond = function(socket) {
-  var chanlist_id = floor(Math.random() * 500);
+  var chanlist_id = Math.floor(Math.random() * 500);
   var Packet = Buffer.from('({"chanlist-reply",5,"*nodejs",0,"DeadSoulsNew",0,' + chanlist_id +  ',(["slack":({"nodejs","1",}),]),})\0', 'ascii')
   var PacketLength = Buffer.alloc(4)
   PacketLength.writeInt32BE(Packet.length)
